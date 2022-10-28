d6ecd7e91aab709937c366e03b5038eef0e14d32
Chijndu <nwanneemmanuella@gmail.com>
2022-10-27 17:02:52 -0700

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,12 +3,12 @@
 /**
  * rot13 - encodes a string using rot13
  * @str:the string to be encoded.
- * return: a pointer to the encoded string
+ * Return: a pointer to the encoded string
  */
 char *rot13(char *str)
 {
 	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 		'G', 'H', 'I', 'J', 'K', 'L',
 		'M', 'N', 'O', 'P', 'Q', 'R',
 		'S', 'T', 'U', 'V', 'W', 'X',
