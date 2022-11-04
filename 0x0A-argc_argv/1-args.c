035d2cfb0ea03ee3949746c9bf948a33dafaaa24
Chijndu <nwanneemmanuella@gmail.com>
2022-11-04 13:26:23 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,6 +3,7 @@
 /**
  * main - a program what prints the # of args passed to it
  * @argc: the # of args
+ * @argv: the names
  * Return: 0
  */
 int main(int argc, _attribute_((unused))char **argv)
